dbdd35ea5d6f31325ab6204feadc0b0654fe7270
RayceWheat <RayceWheat@gmail.com>
2022-07-12 15:12:11 -0500

    intial spring package

---
+++ Group2GameStore/src/test/java/com/company/Group2GameStore/Group2GameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Group2GameStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group2GameStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
