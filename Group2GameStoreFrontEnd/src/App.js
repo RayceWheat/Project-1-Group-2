1616b403989139bb9ed00ed2c2aa2a2aff3d7e1a
Twigster <treshagaye@gmail.com>
2022-07-15 17:23:07 -0400

    invoice files added

--- Group2GameStoreFrontEnd/src/App.js
+++ Group2GameStoreFrontEnd/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Games from './Games';
+import Invoice from './Invoice';
 
 function App() {
   return (
     <main>
       <Games />
+      <Invoice />
+
     </main>
   );
 }
